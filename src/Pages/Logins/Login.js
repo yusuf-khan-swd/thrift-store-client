fcb2cd83152cf325d79c9b2895e73f283c20a102
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-24 11:51:57 +0600

    Added style to register titile

--- src/Pages/Logins/Login.js
+++ src/Pages/Logins/Login.js
@@ -5,9 +5,7 @@ const Login = () => {
   return (
     <div>
       <h2 className='text-2xl font-bold'>Login</h2>
-      <p>
-        New to Thrift Store? Please <Link to="/register" className='text-blue-600'> create an account.</Link>
-      </p>
+
     </div>
   );
 };
