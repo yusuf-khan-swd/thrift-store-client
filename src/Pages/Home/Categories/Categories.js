329f935771a2381db5ec521d64b1469d83f03fb7
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-25 20:09:33 +0600

    added category card and style

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -19,11 +19,13 @@ const Categories = () => {
   }
 
   return (
-    <div>
-      <h2>Categories {categories.length} </h2>
-      {
-        categories.map(category => <CategoryCard key={category._id} category={category}></CategoryCard>)
-      }
+    <div className='container mx-auto'>
+      <h2 className='text-3xl font-bold text-center mb-8'>Categories</h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        {
+          categories.map(category => <CategoryCard key={category._id} category={category}></CategoryCard>)
+        }
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Categories/CategoryCard.js
+++ src/Pages/Home/Categories/CategoryCard.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 const CategoryCard = ({ category }) => {
+  const { name, image } = category;
 
   return (
     <div>
-      <h2>Category</h2>
+      <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <figure><img src={image} className="w-full h-80" alt={name} /></figure>
+        <div className="card-body">
+          <h2 className="card-title uppercase">{name}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
