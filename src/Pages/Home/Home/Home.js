a238224765064a643afb4d8d1157b8278fece05c
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-26 01:07:06 +0600

    added extra section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Categories from '../Categories/Categories';
+import OurStates from '../OurStats/OurStates';
 
 const Home = () => {
   return (
     <div className='container mx-auto'>
       <Banner></Banner>
       <Categories></Categories>
+      <OurStates></OurStates>
     </div>
   );
 };
---
+++ src/Pages/Home/OurStats/OurStates.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const OurStates = () => {
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 bg-white">
+      <div className="max-w-xl mb-10 md:mx-auto text-center lg:max-w-2xl md:mb-12">
+        <div>
+          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-400">
+            our all statistic
+          </p>
+        </div>
+        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          The quick, brown fox jumps over a lazy dog
+        </h2>
+        <p className="text-base text-gray-700 md:text-lg">
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque rem aperiam, eaque ipsa quae.
+        </p>
+      </div>
+      <div className="relative w-full p-px mx-auto mb-4 overflow-hidden transition-shadow duration-300 border rounded lg:mb-8 lg:max-w-4xl group hover:shadow-xl">
+        <div className="absolute bottom-0 left-0 w-full h-1 duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
+        <div className="absolute bottom-0 left-0 w-1 h-full duration-300 origin-bottom transform scale-y-0 bg-deep-purple-accent-400 group-hover:scale-y-100" />
+        <div className="absolute top-0 left-0 w-full h-1 duration-300 origin-right transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100" />
+        <div className="absolute bottom-0 right-0 w-1 h-full duration-300 origin-top transform scale-y-0 bg-deep-purple-accent-400 group-hover:scale-y-100" />
+        <div className="relative flex flex-col items-center h-full py-10 duration-300 bg-white rounded-sm transition-color sm:items-stretch sm:flex-row">
+          <div className="px-12 py-8 text-center">
+            <h6 className="text-4xl font-bold text-deep-purple-accent-400 sm:text-5xl">
+              82%
+            </h6>
+            <p className="text-center md:text-base">
+              Goes to our charity fund along with our employee salary
+            </p>
+          </div>
+          <div className="w-56 h-1 transition duration-300 transform bg-gray-300 rounded-full group-hover:bg-deep-purple-accent-400 group-hover:scale-110 sm:h-auto sm:w-1" />
+          <div className="px-12 py-8 text-center">
+            <h6 className="text-4xl font-bold text-deep-purple-accent-400 sm:text-5xl">
+              106.5K
+            </h6>
+            <p className="text-center md:text-base">
+              Done Donation to charity organization
+            </p>
+          </div>
+        </div>
+      </div>
+      <p className="mx-auto mb-4 text-gray-600 sm:text-center lg:max-w-2xl lg:mb-6 md:px-16">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+  );
+};
+
+export default OurStates;
