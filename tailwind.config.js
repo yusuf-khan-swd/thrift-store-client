e60521327d37bba8bb2d0dc1fc87d616cc86b1a2
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-25 22:24:25 +0600

    added daisyui theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,22 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#2dd4bf",
+          secondary: "#34d399",
+          accent: "#37CDBE",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 }
