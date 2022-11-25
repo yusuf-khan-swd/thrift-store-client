3f29a2bd30aecf4cfa8cd8803f4a15addbdaa585
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-25 18:26:18 +0600

    added react query

---
+++ src/Pages/Home/Categories/Categories.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+const Categories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => { }, []);
+
+  return (
+    <div>
+      <h2>Categories</h2>
+    </div>
+  );
+};
+
+export default Categories;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Categories from '../Categories/Categories';
 
 const Home = () => {
   return (
     <div className='container mx-auto'>
       <h2 className='text-2xl font-semibold'>Home Page</h2>
+      <Categories></Categories>
     </div>
   );
 };
