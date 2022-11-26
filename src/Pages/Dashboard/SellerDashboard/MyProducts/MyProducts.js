d19d4724ac9a303ef4b46260244211be1fdfd46c
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-26 16:16:36 +0600

    format with prettier

--- src/Pages/Dashboard/SellerDashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/SellerDashboard/MyProducts/MyProducts.js
@@ -1,54 +1,67 @@
-import { useQuery } from '@tanstack/react-query';
-import React, { useContext } from 'react';
-import toast from 'react-hot-toast';
-import { AuthContext } from '../../../../contexts/AuthProvider/AuthProvider';
-import Loading from '../../../Shared/Loading/Loading';
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext } from "react";
+import toast from "react-hot-toast";
+import { AuthContext } from "../../../../contexts/AuthProvider/AuthProvider";
+import Loading from "../../../Shared/Loading/Loading";
 
 const MyProducts = () => {
   const { user } = useContext(AuthContext);
 
-  const { data: products, isLoading, refetch } = useQuery({
+  const {
+    data: products,
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: ["products"],
     queryFn: async () => {
-      const res = await fetch(`http://localhost:5000/products?email=${user.email}`, {
-        headers: {
-          authorization: `bearer ${localStorage.getItem("thrift-token")}`
+      const res = await fetch(
+        `http://localhost:5000/products?email=${user.email}`,
+        {
+          headers: {
+            authorization: `bearer ${localStorage.getItem("thrift-token")}`,
+          },
         }
-      });
+      );
       const data = await res.json();
       return data;
-    }
+    },
   });
 
   if (isLoading) {
-    return <Loading></Loading>
+    return <Loading></Loading>;
   }
 
   if (!products.length) {
-    return <div className='h-screen flex justify-center items-center lg:items-start lg:mt-8'>
-      <h2 className="text-5xl font-bold">Your Have <span className='text-teal-500'>0</span> Items</h2>
-    </div>
+    return (
+      <div className="h-screen flex justify-center items-center lg:items-start lg:mt-8">
+        <div className="text-5xl font-bold">
+          Your Have <span className="text-teal-500">0</span> Items on Sale
+        </div>
+      </div>
+    );
   }
 
   const handleAdvertised = (id) => {
     fetch(`http://localhost:5000/products/${id}`, {
-      method: 'PUT',
+      method: "PUT",
       headers: {
-        'content-type': 'application/json'
-      }
+        "content-type": "application/json",
+      },
     })
-      .then(res => res.json())
-      .then(data => {
+      .then((res) => res.json())
+      .then((data) => {
         if (data.modifiedCount) {
           toast.success("Product on advertised list.");
           refetch();
         }
-      })
+      });
   };
 
   return (
     <div>
-      <h2 className='text-3xl font-bold text-center cursor-pointer underline underline-offset-4 py-8 uppercase'>My Products {products.length}</h2>
+      <h2 className="text-3xl font-bold text-center cursor-pointer underline underline-offset-4 py-8 uppercase">
+        My Products {products.length}
+      </h2>
       <div className="overflow-x-auto m-2 lg:m-5">
         <div className="overflow-x-auto w-full">
           <table className="table w-full">
@@ -63,32 +76,44 @@ const MyProducts = () => {
               </tr>
             </thead>
             <tbody>
-              {
-                products.map((product, index) =>
-                  <tr key={product._id}>
-                    <th>{index}</th>
-                    <td>
-                      <div className="flex items-center space-x-3">
-                        <div className="avatar">
-                          <div className="mask mask-squircle w-12 h-12">
-                            <img src={product.image} alt="Avatar Tailwind CSS Component" />
-                          </div>
-                        </div>
-                        <div>
-                          <div className="font-bold">{product.productName}</div>
+              {products.map((product, index) => (
+                <tr key={product._id}>
+                  <th>{index}</th>
+                  <td>
+                    <div className="flex items-center space-x-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img
+                            src={product.image}
+                            alt="Avatar Tailwind CSS Component"
+                          />
                         </div>
                       </div>
-                    </td>
-                    <td>${product.resalePrice}</td>
-                    <td className='uppercase font-medium'>{product.saleStatus}</td>
-                    <th>
-                      <button onClick={() => handleAdvertised(product._id)} className="btn btn-primary btn-xs" disabled={product.advertised}>Advertise</button>
-                    </th>
-                    <td>
-                      <button className='btn btn-error btn-sm text-gray-600 font-bold'>Delete</button>
-                    </td>
-                  </tr>)
-              }
+                      <div>
+                        <div className="font-bold">{product.productName}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>${product.resalePrice}</td>
+                  <td className="uppercase font-medium">
+                    {product.saleStatus}
+                  </td>
+                  <th>
+                    <button
+                      onClick={() => handleAdvertised(product._id)}
+                      className="btn btn-primary btn-xs"
+                      disabled={product.advertised}
+                    >
+                      Advertise
+                    </button>
+                  </th>
+                  <td>
+                    <button className="btn btn-error btn-sm text-gray-600 font-bold">
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
