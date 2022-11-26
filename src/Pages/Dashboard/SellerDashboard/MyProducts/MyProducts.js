5d4d7c0b520dd56959c949eb606b1c4abe9e15ec
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-26 23:48:31 +0600

    added a spinner

--- src/Pages/Dashboard/SellerDashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/SellerDashboard/MyProducts/MyProducts.js
@@ -1,10 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import toast from "react-hot-toast";
 import { AuthContext } from "../../../../contexts/AuthProvider/AuthProvider";
 import Loading from "../../../Shared/Loading/Loading";
+import Spinner from "../../../Shared/Spinner/Spinner";
 
 const MyProducts = () => {
+  const [isDataLoading, setIsDataLoading] = useState(false);
   const { user } = useContext(AuthContext);
 
   const {
@@ -42,6 +44,7 @@ const MyProducts = () => {
   }
 
   const handleAdvertised = (id, advertise) => {
+    setIsDataLoading(true);
     fetch(`http://localhost:5000/products/${id}`, {
       method: "PUT",
       headers: {
@@ -55,6 +58,7 @@ const MyProducts = () => {
         if (data.modifiedCount) {
           toast.success(`${advertise ? 'Product remove from advertised list' : 'Product on advertised list.'}`);
           refetch();
+          setIsDataLoading(false);
         }
       });
   };
@@ -66,6 +70,7 @@ const MyProducts = () => {
       return;
     }
 
+    setIsDataLoading(true);
     fetch(`http://localhost:5000/products/${id}`, {
       method: 'DELETE',
       headers: {
@@ -77,6 +82,7 @@ const MyProducts = () => {
         if (data.deletedCount) {
           toast.success("Your product remove from sales");
           refetch();
+          setIsDataLoading(false);
         }
       })
   };
@@ -86,6 +92,12 @@ const MyProducts = () => {
       <h2 className="text-3xl font-bold text-center cursor-pointer underline underline-offset-4 py-8 uppercase">
         My Products: <span className="text-teal-500">{products.length}</span>
       </h2>
+      <div className="h-8">
+        {
+          isDataLoading &&
+          <Spinner></Spinner>
+        }
+      </div>
       <div className="overflow-x-auto m-2 lg:m-5">
         <div className="overflow-x-auto w-full">
           <table className="table w-full">
