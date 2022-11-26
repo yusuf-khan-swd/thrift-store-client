c7b6b1fd88fe7db23f0056239dfe656cc5893354
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-26 15:54:45 +0600

    show seller added product in table

--- src/Pages/Dashboard/SellerDashboard/MyProducts/MyProducts.js
+++ src/Pages/Dashboard/SellerDashboard/MyProducts/MyProducts.js
@@ -23,8 +23,6 @@ const MyProducts = () => {
     return <Loading></Loading>
   }
 
-  console.log(products);
-
   if (!products.length) {
     return <div className='h-screen flex justify-center items-center lg:items-start lg:mt-8'>
       <h2 className="text-5xl font-bold">Your Have <span className='text-teal-500'>0</span> Items</h2>
@@ -33,7 +31,51 @@ const MyProducts = () => {
 
   return (
     <div>
-      <h2>My products </h2>
+      <h2 className='text-3xl font-bold text-center cursor-pointer underline underline-offset-4 py-8 uppercase'>My Products {products.length}</h2>
+      <div className="overflow-x-auto m-2 lg:m-5">
+        <div className="overflow-x-auto w-full">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Sales Status</th>
+                <th>Advertise Status</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                products.map((product, index) =>
+                  <tr key={product._id}>
+                    <th>{index}</th>
+                    <td>
+                      <div className="flex items-center space-x-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle w-12 h-12">
+                            <img src={product.image} alt="Avatar Tailwind CSS Component" />
+                          </div>
+                        </div>
+                        <div>
+                          <div className="font-bold">{product.productName}</div>
+                        </div>
+                      </div>
+                    </td>
+                    <td>${product.resalePrice}</td>
+                    <td className='uppercase font-medium'>{product.saleStatus}</td>
+                    <th>
+                      <button className="btn btn-primary btn-xs">Advertise</button>
+                    </th>
+                    <td>
+                      <button className='btn btn-error btn-sm text-gray-600 font-bold'>Delete</button>
+                    </td>
+                  </tr>)
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
