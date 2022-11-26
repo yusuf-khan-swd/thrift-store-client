3f8879f056abe31668a05e05a4773c7008780618
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-26 22:39:49 +0600

    added all buyers section

--- src/Pages/Dashboard/AdminDashboard/AllBuyers/AllBuyers.js
+++ src/Pages/Dashboard/AdminDashboard/AllBuyers/AllBuyers.js
@@ -1,9 +1,85 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import toast from 'react-hot-toast';
+import Loading from '../../../Shared/Loading/Loading';
 
 const AllBuyers = () => {
+  const { data: buyers, isLoading, refetch } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/all-buyers", {
+        headers: {
+          authorization: `bearer ${localStorage.getItem('thrift-token')}`
+        }
+      });
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
+  if (!buyers.length) {
+    return (
+      <div className="h-screen flex justify-center items-center lg:items-start lg:mt-8">
+        <h2 className="text-5xl font-bold">
+          Total <span className="text-teal-500">0</span> register buyers.
+        </h2>
+      </div>
+    );
+  }
+
+  const handleDeleteBuyers = (id) => {
+    fetch(`http://localhost:5000/all-buyers/${id}`, {
+      method: 'DELETE',
+      headers: {
+        authorization: `bearer ${localStorage.getItem("thrift-token")}`
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.deletedCount) {
+          toast.success("User deleted successfully.");
+          refetch();
+        }
+      })
+  };
+
+
   return (
     <div>
-      <h2>All Buyers</h2>
+      <div className="text-center py-8">
+        <h2 className="text-3xl font-bold uppercase cursor-pointer">
+          <span className="underline">All Buyers:</span> <span className="text-teal-400">{buyers.length}</span>
+        </h2>
+      </div>
+      <div className="overflow-x-auto m-2 lg:m-5">
+        <table className="table table-zebra w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Buyer Name</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              buyers.map((buyer, index) => <tr key={buyer._id}>
+                <th>{(index + 1) < 10 ? ('0' + (index + 1)) : (index + 1)}</th>
+                <td>{buyer.userName}</td>
+                <td>{buyer.userEmail}</td>
+                <td>
+                  <button onClick={() => handleDeleteBuyers(buyer._id)} className="btn btn-error btn-xs text-gray-600 font-bold mr-4">Delete</button>
+                </td>
+              </tr>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
