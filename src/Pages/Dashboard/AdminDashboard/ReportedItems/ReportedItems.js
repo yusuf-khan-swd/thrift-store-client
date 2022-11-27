2eaa93ddcc8e6f242596ec4a7f5bf6f3a5e88baa
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-28 01:22:21 +0600

    show reported product to admin

--- src/Pages/Dashboard/AdminDashboard/ReportedItems/ReportedItems.js
+++ src/Pages/Dashboard/AdminDashboard/ReportedItems/ReportedItems.js
@@ -1,8 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
 import Loading from '../../../Shared/Loading/Loading';
+import Spinner from '../../../Shared/Spinner/Spinner';
 
 const ReportedItems = () => {
+  const [isDataLoading, setIsDataLoading] = useState(false);
+
   const { data: reports, isLoading } = useQuery({
     queryKey: ['products'],
     queryFn: async () => {
@@ -20,11 +23,57 @@ const ReportedItems = () => {
     return <Loading></Loading>;
   }
 
-  console.log(reports)
+  if (!reports.length) {
+    return (
+      <div className="h-screen flex justify-center items-center lg:items-start lg:mt-8">
+        <h2 className="text-5xl font-bold">
+          No product reported.
+        </h2>
+      </div>
+    );
+  }
+
+  const handleDeleteProducts = (id) => {
+
+  };
 
   return (
     <div>
-      <h2>Reported Items</h2>
+      <div className="text-center py-8">
+        <h2 className="text-3xl font-bold uppercase cursor-pointer text-teal-400 underline">
+          Total reported product - {reports.length}
+        </h2>
+      </div>
+      <div className="h-8">{isDataLoading && <Spinner></Spinner>}</div>
+      <div className="overflow-x-auto m-2 lg:m-5">
+        <table className="table table-zebra w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Reported Product Name</th>
+              <th>Email</th>
+              <th>Seller Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reports.map((report, index) => (
+              <tr key={report._id}>
+                <th>{index + 1 < 10 ? "0" + (index + 1) : index + 1}</th>
+                <td>{report.productName}</td>
+                <td>{report.sellerEmail}</td>
+                <td>
+                  <button
+                    onClick={() => handleDeleteProducts(report._id)}
+                    className="btn btn-error btn-xs text-gray-600 font-bold mr-4"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
