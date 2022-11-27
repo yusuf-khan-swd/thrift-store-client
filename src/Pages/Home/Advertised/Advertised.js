d5beac127842b966bbd0eaee4692807eb0bce7d2
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-27 18:56:56 +0600

    show advertised items

--- src/Pages/Home/Advertised/Advertised.js
+++ src/Pages/Home/Advertised/Advertised.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AdvertisedCard from './AdvertisedCard';
 
 const Advertised = ({ advertisedItems }) => {
 
@@ -6,7 +7,12 @@ const Advertised = ({ advertisedItems }) => {
 
   return (
     <div>
-      <h2>Advertised Items {advertisedItems.length} </h2>
+      <h2 className='text-3xl font-bold text-center my-8 uppercase underline cursor-pointer'>Advertised Items {advertisedItems.length} </h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        {
+          advertisedItems.map(item => <AdvertisedCard key={item._id} item={item}></AdvertisedCard>)
+        }
+      </div>
     </div>
   );
 };
