cd4b8ff7c59f1b43b77bb2164c81db6fb2d32898
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-27 18:28:01 +0600

    added advertised items and show only if any product advertised

---
+++ src/Pages/Home/Advertised/Advertised.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Advertised = ({ advertisedItems }) => {
+
+  console.log(advertisedItems);
+
+  return (
+    <div>
+      <h2>Advertised Items</h2>
+    </div>
+  );
+};
+
+export default Advertised;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,13 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
+import Loading from '../../Shared/Loading/Loading';
+import Advertised from '../Advertised/Advertised';
 import Banner from '../Banner/Banner';
 import Categories from '../Categories/Categories';
 import OurStates from '../OurStats/OurStates';
 
 const Home = () => {
+  const { data: advertisedItems, isLoading } = useQuery({
+    queryKey: ["advertised"],
+    queryFn: async () => {
+      const res = await axios.get("http://localhost:5000/advertised");
+      const data = await res.data;
+      return data;
+    }
+  });
+
+  if (isLoading) {
+    return <Loading></Loading>
+  }
+
   return (
     <div className='container mx-auto'>
       <Banner></Banner>
       <Categories></Categories>
+      {
+        advertisedItems.length !== 0 &&
+        <Advertised advertisedItems={advertisedItems}></Advertised>
+      }
       <OurStates></OurStates>
     </div>
   );
