8c7fe0c53ecb61457181d3e5ad9c01e350904d06
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-27 13:02:55 +0600

    added product information

--- src/Pages/Products/ProductCard.js
+++ src/Pages/Products/ProductCard.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
 const ProductCard = ({ product }) => {
-  const { productName, image, } = product;
+  const { productName, image, location, resalePrice, originalPrice, monthsUsed, time, sellerName, sellerIsVerified, conditionType, sellerNumber, description } = product;
   return (
     <div className='m-2'>
-      <div className="card card-side bg-base-100 shadow-xl border">
+      <div className="card card-side bg-white shadow-xl border">
         <div>
-          <figure><img src={image} className="w-1/2 " alt={productName} /></figure>
+          <figure><img src={image} className="w-1/2" alt={productName} /></figure>
         </div>
         <div className="card-body">
           <h2 className="card-title uppercase justify-center">{productName}</h2>
-          <p></p>
+          <p>location: {location} </p>
+          <p>Resale Price: ${resalePrice} </p>
+          <p>Original Price: ${originalPrice}</p>
+          <p>Months of Use: ${monthsUsed} </p>
+          <p>Posted Time: {time}</p>
+          <p>Seller Name: {sellerName}</p>
+          <p>Seller is verified: {`${sellerIsVerified}`} </p>
+          <p>Condition Type: {conditionType}</p>
+          <p>Mobile Number: {sellerNumber}</p>
+          <p>Description: {description}</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Book now</button>
+          </div>
         </div>
       </div>
     </div>
