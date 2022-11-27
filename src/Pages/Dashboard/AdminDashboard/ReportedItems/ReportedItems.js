690d6342e6673d4cda7e81f7a58afc8e67102995
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-28 01:06:10 +0600

    call report api

--- src/Pages/Dashboard/AdminDashboard/ReportedItems/ReportedItems.js
+++ src/Pages/Dashboard/AdminDashboard/ReportedItems/ReportedItems.js
@@ -1,6 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const ReportedItems = () => {
+  const { } = useQuery({
+    queryKey: ['products'],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/products")
+    }
+  })
   return (
     <div>
       <h2>Reported Items</h2>
