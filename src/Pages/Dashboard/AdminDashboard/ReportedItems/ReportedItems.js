38241cfc47932b57c15c8f889af70c2ae97e8c21
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-28 01:14:19 +0600

    load reported item

--- src/Pages/Dashboard/AdminDashboard/ReportedItems/ReportedItems.js
+++ src/Pages/Dashboard/AdminDashboard/ReportedItems/ReportedItems.js
@@ -1,13 +1,27 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Loading from '../../../Shared/Loading/Loading';
 
 const ReportedItems = () => {
-  const { } = useQuery({
+  const { data: reports, isLoading } = useQuery({
     queryKey: ['products'],
     queryFn: async () => {
-      const res = await fetch("http://localhost:5000/products")
+      const res = await fetch("http://localhost:5000/reports", {
+        headers: {
+          authorization: `bearer ${localStorage.getItem("thrift-token")}`
+        }
+      });
+      const data = await res.json();
+      return data;
     }
   })
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
+  console.log(reports)
+
   return (
     <div>
       <h2>Reported Items</h2>
