6c0aca3d540459c1a0597e39a26e000f7fe4db0b
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-27 16:28:55 +0600

    added back to categories

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import BookModal from "./BookModal";
 import ProductCard from "./ProductCard";
 
@@ -16,8 +16,8 @@ const Products = () => {
 
   return (
     <div className="container mx-auto mb-24">
-      <h2 className="text-3xl font-bold text-center my-8 uppercase underline cursor-pointer">
-        <span className="text-sm">Category</span> <br /> <span>{products[0]?.productCategory}</span>
+      <h2 className="text-3xl font-bold text-center my-8 uppercase">
+        <Link to="/categories" className="text-sm">Category</Link> <br /> <span>{products[0]?.productCategory}</span>
       </h2>
       <div className="grid grid-cols-1 gap-6 ">
         {products.map((product) => (
