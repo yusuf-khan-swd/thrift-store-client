fd79bf4bf041ab7b46fb5fbb3b13c72a8589499a
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-27 12:15:30 +0600

    conditionally show drawer icon if location is in dashboard

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Products = () => {
-  const abc = useLoaderData();
-  console.log(abc);
+  const allProducts = useLoaderData();
+  console.log(allProducts);
 
   return (
-    <div>
-      <h2>This is products</h2>
+    <div className='container mx-auto'>
+      <h2 className='text-3xl font-bold text-center my-8 uppercase underline cursor-pointer'>This is {allProducts[0]?.productCategory} Category</h2>
     </div>
   );
 };
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react';
 import toast from 'react-hot-toast';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
+  const currentLocation = useLocation().pathname;
+  const location = currentLocation === "/dashboard";
+
 
   const handleLogOut = () => {
     logOut()
@@ -47,7 +50,7 @@ const Navbar = () => {
           <Link to="/" className="btn btn-ghost normal-case text-xl">Thrift Store</Link>
         </div>
         {
-          user?.uid &&
+          user?.uid && location &&
           <div className='navbar-end'>
             <label htmlFor="dashboard-drawer" tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
