5e7b4934a3e6e27d7cc8d989efc0db8bd1699f22
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-28 11:27:13 +0600

    added some information of product to advertised

--- src/Pages/Home/Advertised/AdvertisedCard.js
+++ src/Pages/Home/Advertised/AdvertisedCard.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const AdvertisedCard = ({ item, setOpenModal,
   handleBookProduct, }) => {
-  const { productName, image } = item;
+  const { productName, image, resalePrice, location, monthsUsed } = item;
 
   const handleBooked = (modal, product) => {
     setOpenModal(modal);
@@ -16,6 +16,9 @@ const AdvertisedCard = ({ item, setOpenModal,
         </figure>
         <div className="card-body">
           <h2 className="card-title uppercase justify-center">{productName}</h2>
+          <p className="font-bold">Resale price: ${resalePrice}</p>
+          <p>Location: {location}</p>
+          <p>Used: {monthsUsed} Month</p>
           <div className="card-actions justify-center">
             <label
               onClick={() => handleBooked(true, item)}
