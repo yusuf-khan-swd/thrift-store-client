d31c613ca2d2397ceb9cc71e87cad329e6e4929f
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-28 17:14:13 +0600

    rename the site name

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -47,7 +47,7 @@ const Navbar = () => {
               {menuItems}
             </ul>
           </div>
-          <Link to="/" className="btn btn-ghost normal-case text-xl">Thrift Store</Link>
+          <Link to="/" className="btn btn-ghost normal-case text-xl">Charity Truly</Link>
         </div>
         {
           user?.uid && location &&
