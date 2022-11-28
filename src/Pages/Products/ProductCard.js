8b5f11f1a33f1d83ba61eb9bbc4f08647208f16c
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-11-28 18:52:22 +0600

    added style to card

--- src/Pages/Products/ProductCard.js
+++ src/Pages/Products/ProductCard.js
@@ -41,31 +41,34 @@ const ProductCard = ({
 
   return (
     <div className="m-2">
-      <div className="card grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-white shadow-xl rounded-lg">
+      <div className="card grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 bg-white shadow-xl rounded-lg max-w-6xl mx-auto">
         <figure>
-          <img src={image} className="p-3 rounded-3xl" alt={productName} />
+          <img src={image} className="p-2 rounded-2xl" alt={productName} />
         </figure>
-        <div className="card-body lg:col-span-2">
-          <h2 className="card-title uppercase justify-center">{productName}</h2>
-          <p>Location: {location} </p>
-          <p>Resale Price: ${resalePrice} </p>
-          <p>Original Price: ${originalPrice}</p>
-          <p>Months of Use: ${monthsUsed} </p>
-          <p>Posted Time: {postedDate} {hour}{AmOrPm}</p>
-          <p className="flex items-center">
-
-            Seller Name: {sellerName}
-            {sellerIsVerified && (
-              <FaCheckCircle
-                className="ml-3"
-                title="Seller is verified"
-              ></FaCheckCircle>
-            )}
-          </p>
-          <p>Condition Type: {conditionType}</p>
-          <p>Mobile Number: {sellerNumber}</p>
-          <p>Description: {description}</p>
-          <div className="card-actions justify-end">
+        <div className="card-body lg:col-span-2 text-zinc-500">
+          <h2 className="card-title uppercase justify-center font-bold">{productName}</h2>
+          <p className="font-semibold text-xl text-center mb-5">Resale Price: ${resalePrice} </p>
+          <div className="mb-3">
+            <p>Months of Use: {monthsUsed} Month</p>
+            <p>Original Price: ${originalPrice}</p>
+            <p>Location: {location} </p>
+            <p>Posted Time: {postedDate} {hour}{AmOrPm}</p>
+            <p>Condition Type: {conditionType}</p>
+          </div>
+          <div>
+            <p className="flex items-center">
+              Seller Name: {sellerName}
+              {sellerIsVerified && (
+                <FaCheckCircle
+                  className="ml-3 text-blue-600"
+                  title="Seller is verified"
+                ></FaCheckCircle>
+              )}
+            </p>
+            <p>Seller Mobile Number: {sellerNumber}</p>
+            <p className="mb-3"><span className="font-medium">Description:</span> {description}</p>
+          </div>
+          <div className="card-actions justify-start lg:justify-end mt-5">
             <label
               onClick={() => handleBooked(true, product)}
               htmlFor="book-modal"
