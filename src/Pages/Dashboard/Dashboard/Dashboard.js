9ef47d67227a9bb3f98d5baa5df92b0c14a94f47
yusuf-khan-swd <ysufkhan.swd@gmail.com>
2022-12-02 17:30:08 +0600

    modified dashboard

--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -5,24 +5,26 @@ const Dashboard = () => {
   const { user } = useContext(AuthContext);
 
   return (
-    <div className='flex flex-col h-screen justify-center items-center md:block'>
-      <div className='card w-96 max-w-sm shadow-lg bg-white md:mt-16 md:ml-16 border'>
-        <div className='card-body'>
-          <h2 className='card-title justify-center text-2xl cursor-pointer'>Profile</h2>
-          <form>
-            <div className="form-control w-full">
-              <label className="label">
-                <span className="label-text font-medium">Name</span>
-              </label>
-              <input type="text" defaultValue={user.displayName} disabled className="input input-bordered w-full" />
-            </div>
-            <div className="form-control w-full">
-              <label className="label">
-                <span className="label-text font-medium">Email</span>
-              </label>
-              <input type="text" defaultValue={user.email} disabled className="input input-bordered w-full" />
-            </div>
-          </form>
+    <div className='container mx-auto'>
+      <div className='mt-12 mb-24 m-2'>
+        <div className='card shadow-lg bg-white md:mt-16 md:ml-16 border w-full lg:w-96'>
+          <div className='card-body'>
+            <h2 className='card-title justify-center text-2xl cursor-pointer'>Profile</h2>
+            <form>
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text font-medium">Name</span>
+                </label>
+                <input type="text" defaultValue={user.displayName} disabled className="input input-bordered w-full" />
+              </div>
+              <div className="form-control w-full">
+                <label className="label">
+                  <span className="label-text font-medium">Email</span>
+                </label>
+                <input type="text" defaultValue={user.email} disabled className="input input-bordered w-full" />
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
