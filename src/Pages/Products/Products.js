8e83809f868ca795cf57b5624f2af3fef32ee77f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-12-23 20:14:21 +0600

    fix the position sticky problem for categories

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -81,12 +81,14 @@ const Products = () => {
     <div className="container mx-auto mb-24">
       <div className="m-2 pt-8">
         <section className="grid grid-cols-1 md:grid-cols-5 md:gap-6">
-          <ul className="grid grid-cols-2 sm:gird-cols-3 mb-8 mt-5 md:block md:gap-0 md:my-0 menu">
-            <CategoryName
-              listStyle={`border rounded-lg m-1`}
-              linkStyle={`rounded-lg`}
-            ></CategoryName>
-          </ul>
+          <div>
+            <ul className="grid grid-cols-2 sm:gird-cols-3 mb-8 mt-5 md:block md:gap-0 md:my-0 menu sticky top-16">
+              <CategoryName
+                listStyle={`border rounded-lg m-1`}
+                linkStyle={`rounded-lg`}
+              ></CategoryName>
+            </ul>
+          </div>
           <div className="col-span-4">
             <div className="grid grid-cols-1 gap-6">
               {products.map((product) => (
