93da3a07671823ece41cfe221e4f6dda81e945f7
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-12-23 18:06:53 +0600

    added loading state

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,7 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
 import toast from "react-hot-toast";
-import { Link, useLoaderData, useNavigation, useParams } from "react-router-dom";
+import { Link, useLoaderData, useNavigation } from "react-router-dom";
 import CategoryName from "../Shared/CategoryName/CategoryName";
 import Loading from "../Shared/Loading/Loading";
 import BookModal from "./BookModal";
@@ -11,15 +10,9 @@ const Products = () => {
   const [openModal, setOpenModal] = useState(true);
   const [isDataLoading, setIsDataLoading] = useState(false);
   const [productBooked, setProductBooked] = useState({});
-  const { id } = useParams();
 
   const products = useLoaderData();
-
   const navigation = useNavigation();
-
-  if (navigation.state === "loading") {
-    return <Loading></Loading>
-  }
 
   if (!products.length) {
     return (
@@ -49,8 +42,7 @@ const Products = () => {
       .then((res) => res.json())
       .then((data) => {
         if (data.modifiedCount) {
-          toast.success(`${reported ? "Reported to admin" : "Remove report"}`);
-          // refetch();
+          toast.success(`${reported ? "Remove report" : "Reported to admin"}`);
           setIsDataLoading(false);
         }
       })
@@ -62,30 +54,37 @@ const Products = () => {
   return (
     <div className="container mx-auto mb-24">
       <div className="m-2">
-        <div className="pb-0">
-          <h2 className="text-3xl font-bold text-center mb-2 uppercase">
-            <Link to="/categories" className="text-sm text-info hover:link">
-              Category
-            </Link>
-            <br /> <span>{products[0]?.productCategory}</span>
-          </h2>
-          <p className="text-center font-bold text-zinc-400 text-xl mb-5">Total {products.length} Products Available</p>
-        </div>
-        <section className="grid grid-cols-5">
+        <section className="grid grid-cols-5 ">
           <ul>
             <CategoryName listMargin={`m-1`} linkStyle={``}></CategoryName>
           </ul>
-          <div className="grid grid-cols-1 gap-6 col-span-4">
-            {products.map((product) => (
-              <ProductCard
-                key={product._id}
-                product={product}
-                setOpenModal={setOpenModal}
-                handleBookProduct={handleBookProduct}
-                handleReport={handleReport}
-                isDataLoading={isDataLoading}
-              ></ProductCard>
-            ))}
+          <div className="col-span-4">
+            <div className="pb-0">
+              <h2 className="text-3xl font-bold text-center mb-2 uppercase">
+                <Link to="/categories" className="text-sm text-info hover:link">
+                  Category
+                </Link>
+                <br /> <span>{products[0]?.productCategory}</span>
+              </h2>
+              <p className="text-center font-bold text-zinc-400 text-xl mb-5">Total {products.length} Products Available</p>
+            </div>
+            {
+              navigation.state === "loading" ?
+                <Loading style={`h-[50vh]`}></Loading>
+                :
+                <div className="grid grid-cols-1 gap-6">
+                  {products.map((product) => (
+                    <ProductCard
+                      key={product._id}
+                      product={product}
+                      setOpenModal={setOpenModal}
+                      handleBookProduct={handleBookProduct}
+                      handleReport={handleReport}
+                      isDataLoading={isDataLoading}
+                    ></ProductCard>
+                  ))}
+                </div>
+            }
           </div>
         </section>
         <div>
--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Loading = () => {
+const Loading = ({ style }) => {
   return (
-    <div className="flex items-center justify-center space-x-2 h-screen">
+    <div className={`flex items-center justify-center space-x-2 ${style ? style : 'h-screen'}`}>
       <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
       <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
       <div className="w-4 h-4 rounded-full animate-pulse dark:bg-violet-400"></div>
