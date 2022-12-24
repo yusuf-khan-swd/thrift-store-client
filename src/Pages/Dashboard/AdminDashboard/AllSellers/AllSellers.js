c7ec93cde301a21fc4d029f83d0d773f87713d4f
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-12-24 13:39:39 +0600

    remove text color

--- src/Pages/Dashboard/AdminDashboard/AllSellers/AllSellers.js
+++ src/Pages/Dashboard/AdminDashboard/AllSellers/AllSellers.js
@@ -154,7 +154,7 @@ const AllSellers = () => {
                     onClick={() =>
                       handleVerifySeller(seller._id, seller.userIsVerified, seller.userEmail)
                     }
-                    className={`btn btn-xs text-gray-600 font-bold ${seller.userIsVerified ? 'btn-success' : 'btn-primary text-white'}`}
+                    className={`btn btn-xs font-bold ${seller.userIsVerified ? 'btn-success' : 'btn-primary text-white'}`}
                     disabled={isDataLoading}
                   >
                     {seller.userIsVerified ? "Remove Verification" : "Verify"}
