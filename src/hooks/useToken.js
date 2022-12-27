afefc90666e19154b3b328629ace0e791a6ade1c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2022-12-27 19:44:37 +0600

    remove loading state and console log

--- src/hooks/useToken.js
+++ src/hooks/useToken.js
@@ -2,25 +2,21 @@ import { useEffect, useState } from "react";
 
 const useToken = (email) => {
   const [token, setToken] = useState("");
-  const [isTokenLoading, setIsTokenLoading] = useState(false);
 
   useEffect(() => {
-    console.log(email);
     if (email) {
-      setIsTokenLoading(true);
       fetch(`https://thrift-store-server.vercel.app/jwt?email=${email}`)
         .then(res => res.json())
         .then(data => {
           if (data.token) {
             localStorage.setItem("thrift-token", data.token);
             setToken(data.token);
-            setIsTokenLoading(false);
           }
         })
     }
   }, [email]);
 
-  return [token, isTokenLoading]
+  return [token]
 };
 
 export default useToken;
