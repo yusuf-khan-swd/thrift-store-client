1f815c4e0b3064447ed87fa6e24d7271739ac583
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-12-26 22:20:51 +0600

    Hide the dashboard modal in lg screen show only screen less then lg

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -90,30 +90,32 @@ const Navbar = () => {
               Thrift Store
             </Link>
           </div>
-          {currentLocation.includes("/dashboard") && (
-            <div className="navbar-end">
-              <label
-                htmlFor="dashboard-drawer"
-                tabIndex={0}
-                className="btn btn-ghost lg:hidden"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+          <div className="lg:hidden">
+            {currentLocation.includes("/dashboard") && (
+              <div className="navbar-end">
+                <label
+                  htmlFor="dashboard-drawer"
+                  tabIndex={0}
+                  className="btn btn-ghost lg:hidden"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h8m-8 6h16"
-                  />
-                </svg>
-              </label>
-            </div>
-          )}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h8m-8 6h16"
+                    />
+                  </svg>
+                </label>
+              </div>
+            )}
+          </div>
           <div className="navbar navbar-start hidden lg:flex w-3/4">
             <ul className="menu menu-horizontal p-0">{menuItems}</ul>
           </div>
